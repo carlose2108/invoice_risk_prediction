cd8544b2d14bb40c60a0dd1dfe637df71dc1be2b
Carlos Ochoa <carlos.ochoa@augoor.com>
2024-01-14 13:40:56 -0300

    endpoint test removed

--- app/endpoints.py
+++ app/endpoints.py
@@ -21,16 +21,6 @@ def predict(payload: InvoicePayload) -> Dict[str, Any]:
     return predictions
 
 
-@app.post("/process_invoice", response_model=InvoiceRisk, status_code=status.HTTP_200_OK)
-def process_invoice(payload: InvoicePayload) -> Dict[str, Any]:
-    print(payload.invoice_id)
-    print(payload.country)
-    pred = InvoiceRisk(invoice_risk_predictions=[0])
-    pred = pred.model_dump()
-    print(pred)
-    return pred
-
-
 @app.get("/health", status_code=status.HTTP_200_OK)
 def health() -> Dict[str, str]:
     return {"healthcheck": "Everything OK!"}
