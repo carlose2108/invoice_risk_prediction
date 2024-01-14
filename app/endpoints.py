b77a757f0fe5e6e67ea67da99b12a3838e09840b
Carlos Ochoa <carlos.ochoa@augoor.com>
2024-01-14 13:51:05 -0300

    comments added

--- app/endpoints.py
+++ app/endpoints.py
@@ -12,9 +12,11 @@ app = init_app()
 
 @app.post("/invoice_risk", response_model=InvoiceRisk, status_code=status.HTTP_200_OK)
 def predict(payload: InvoicePayload) -> Dict[str, Any]:
+    # Read data
     df = pd.read_csv("data/dataTest.csv")
     invoices_to_predict = df[df["invoiceId"].isin(payload.invoice_id)].drop(columns=["Unnamed: 0", "invoiceId"])
 
+    # Make predictions
     preds = app.model.predict(invoices_to_predict)
     predictions = InvoiceRisk(invoice_risk_predictions=preds)
     predictions = predictions.model_dump()
